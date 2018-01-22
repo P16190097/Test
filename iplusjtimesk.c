8e8fbceac839a3840f9ea576da376bd28de5a39c
P16190097 <35694199+P16190097@users.noreply.github.com>
2018-01-22 14:12:57 +0000

    Add files via upload

---
+++ iplusjtimesk.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, k;
+    printf("Please enter the value for i:\n");
+	scanf("%i",&i);
+	printf("Please enter the value for j:\n");
+	scanf("%i",&j);
+    printf("Please enter the value for k:\n");
+	scanf("%i",&k);
+    printf("i   = %i\n", i);
+    printf("j   = %i\n", j);
+    printf("k   = %i\n", k);
+    printf("i+j*k = %i\n",((i+j)*k));
+    return 0;
+}
